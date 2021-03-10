39096034f62ca40cfb7e0115eb315f320c37788e
AnubhavGoyal-29 <anubhavgoyal2910@gmail.com>
2021-03-10 11:55:22 +0530

    profile activity added

--- StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/loginsignup/registerActivity.java
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/loginsignup/registerActivity.java
@@ -2,7 +2,7 @@ package recreate.india.main.startupcarvaan.loginsignup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import recreate.india.main.startupcarvaan.mainActivities.MainActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -19,8 +19,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import recreate.india.main.startupcarvaan.mainActivities.MainActivity;
 import recreate.india.main.startupcarvaan.R;
 
 public class registerActivity extends AppCompatActivity {
