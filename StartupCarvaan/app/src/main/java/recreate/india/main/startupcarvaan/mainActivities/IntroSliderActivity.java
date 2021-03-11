ada57700a48618c6d4686aaf42ae4d04c2106e1b
Vishnu Bhat <vishnubhat820@gmail.com>
2021-03-11 20:36:15 +0530

    Added and fixed the intro slider activity

---
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/mainActivities/IntroSliderActivity.java
@@ -0,0 +1,170 @@
+package recreate.india.main.startupcarvaan.mainActivities;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import recreate.india.main.startupcarvaan.R;
+
+
+public class IntroSliderActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout mDotLayout;
+    private SliderAdaptar sliderAdaptar;
+    //private ImageView[] mDots;
+    private final int LENGTH_OF_ITEMS = new SliderAdaptar(IntroSliderActivity.this).getCount();
+    private Button prevBtn;
+    private Button nextBtn;
+    private Button skipBtn;
+    private int mCurrentPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_intro_slider);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        prevBtn = findViewById(R.id.prevBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+        skipBtn = findViewById(R.id.skipBtn);
+
+        viewPager = findViewById(R.id.viewPager);
+        mDotLayout = findViewById(R.id.dotLayout);
+
+        sliderAdaptar = new SliderAdaptar(IntroSliderActivity.this);
+
+        viewPager.setAdapter(sliderAdaptar);
+        mDotLayout.setupWithViewPager(viewPager, true);
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mCurrentPage!=LENGTH_OF_ITEMS-1) {
+                    viewPager.setCurrentItem(mCurrentPage + 1);
+                }else {
+                    startActivity(new Intent(IntroSliderActivity.this, recreate.india.main.startupcarvaan.loginsignup.loginActivity.class));
+                    finish();
+                    Log.i("TAG", "ENDDDD");
+                }
+
+            }
+        });
+
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(mCurrentPage - 1);
+            }
+        });
+
+        skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroSliderActivity.this, recreate.india.main.startupcarvaan.loginsignup.loginActivity.class));
+                finish();
+            }
+        });
+
+        mDotLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                mCurrentPage = tab.getPosition();
+
+
+                if(mCurrentPage==0){
+                    nextBtn.setEnabled(true);
+                    prevBtn.setEnabled(false);
+                    prevBtn.setVisibility(View.INVISIBLE);
+                    nextBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setText("Next");
+                }else if(mCurrentPage==LENGTH_OF_ITEMS-1){
+                    nextBtn.setEnabled(true);
+                    prevBtn.setEnabled(true);
+                    prevBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setText("Finish");
+                    prevBtn.setText("Back");
+                }else{
+                    nextBtn.setEnabled(true);
+                    prevBtn.setEnabled(true);
+                    prevBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setVisibility(View.VISIBLE);
+                    nextBtn.setText("Next");
+                    prevBtn.setText("Back");
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+//        addDotsIndicator(0);
+//        viewPager.addOnPageChangeListener(viewListener);
+
+    }
+
+//    public void addDotsIndicator(int position) {
+//
+//        if (mDotLayout != null)
+//            mDotLayout.removeAllViews();
+//        mDots = new ImageView[sliderAdaptar.getCount()];
+//
+//
+//        for (int i = 0; i < mDots.length; i++) {
+//            mDots[i] = new ImageView(getApplicationContext());
+//            if (i==position){
+//                mDots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.selected_dot));
+//            }else{
+//                mDots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(),R.drawable.default_dot));
+//            }
+//            mDotLayout.addView(mDots[i]);
+//        }
+//
+//        //      mDots[position].setTextColor(getResources().getColor(R.color.white));
+//
+//
+//    }
+
+
+//    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+//        @Override
+//        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//        }
+//
+//        @Override
+//        public void onPageSelected(int position) {
+//            addDotsIndicator(position);
+//        }
+//
+//        @Override
+//        public void onPageScrollStateChanged(int state) {
+//
+//        }
+//    };
+
+
+}
---
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/mainActivities/SliderAdaptar.java
@@ -0,0 +1,89 @@
+package recreate.india.main.startupcarvaan.mainActivities;
+
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import recreate.india.main.startupcarvaan.R;
+
+
+public class SliderAdaptar extends PagerAdapter {
+
+    Context mContext;
+    LayoutInflater layoutInflater;
+
+    public SliderAdaptar(Context mContext) {
+        this.mContext = mContext;
+    }
+
+
+    public int[] slide_images = {
+            R.drawable.connect,
+            R.drawable.investing,
+            R.drawable.moneyflow
+    };
+
+    public String[] slide_headings = {
+            "Connect with StartUps",
+            "Invest Without money in StartUps",
+            "Get Real Money"
+
+    };
+
+    public String[] slide_descriptions = {
+            "Feel Free to connect with the new emerging StartUps, get to know about them ,their work,their blogs and to trade with whosoever you'd like",
+            "Invest in the StartUp of your choice without real money  and help in the raising of a StartUp.",
+            "Get real Money from trading in StartUps or by achieving your targets and  get real life benefits also gets a chance to get intern and free lancing in them.  "
+    };
+
+
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImage = view.findViewById(R.id.slideImage);
+        TextView slideHeading = view.findViewById(R.id.slideHeading);
+        TextView slideDescription = view.findViewById(R.id.slideDescription);
+
+        slideImage.setImageResource(slide_images[position]);
+        slideHeading.setText(slide_headings[position]);
+        slideDescription.setText(slide_descriptions[position]);
+
+
+        container.addView(view);
+
+
+
+
+        return view;
+
+    }
+
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
+}
