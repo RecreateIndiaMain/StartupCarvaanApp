87e1e53c9a3bde7a0ec565749ce2bee55f0df9ff
prachi15091999 <18ucs056@lnmiit.ac.in>
2021-05-12 18:52:11 +0530

    l

--- StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/Levels.java
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/Levels.java
@@ -3,12 +3,35 @@ package recreate.india.main.startupcarvaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class Levels extends AppCompatActivity {
-
+ProgressBar pro;
+int status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
+        pro=findViewById(R.id.progressid);
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startProgress();
+            }
+        });
+        thread.start();
+
+    }
+
+    public void startProgress() {
+        status=40;
+        try {
+            Thread.sleep(50);
+            pro.setProgress(status);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
