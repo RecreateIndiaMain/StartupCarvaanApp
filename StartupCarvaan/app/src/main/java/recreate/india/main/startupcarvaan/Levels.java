03e495b7f2ccbb6a5d6e7c10c7655a1a83190757
sidd0203 <siddharthch22@gmail.com>
2021-05-18 17:48:48 +0530

    s

--- StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/Levels.java
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/Levels.java
@@ -5,26 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import java.util.PropertyResourceBundle;
+
 public class Levels extends AppCompatActivity {
-ProgressBar pro;
-int status;
+    ProgressBar pro;
+    int status;
+    private int coins = 250,level;
+    ProgressBar l2,l3,l4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels);
-        pro=findViewById(R.id.progressid);
-        Thread thread=new Thread(new Runnable() {
+        pro = findViewById(R.id.progressid);
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                startProgress();
+//                startProgress();
             }
         });
         thread.start();
+        l2=findViewById(R.id.progressid2);
+        l3=findViewById(R.id.progressid3);
+        l4=findViewById(R.id.progressid4);
+
+        level=coins/100 + 1;
+        if(level>1){
+        pro.setProgress(100);
+        }
+        else
+            pro.setProgress(coins);
+        if(level>2){
+            l2.setProgress(100);
+        }
+        else{
+            l2.setProgress(coins-100);
+        }
+        if(level>3){
+            l3.setProgress(100);
+        }
+        else{
+            l3.setProgress(coins-200);
+        }
+        if(level>4){
+            l4.setProgress(100);
+        }
+        else{
+            l4.setProgress(coins-300);
+        }
+
 
     }
 
     public void startProgress() {
-        status=40;
+        status = 40;
         try {
             Thread.sleep(50);
             pro.setProgress(status);
