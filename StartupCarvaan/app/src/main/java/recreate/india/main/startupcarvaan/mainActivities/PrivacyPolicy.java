4495ae6fca06330c2a9dee123b1ba9854eab04cf
prachi15091999 <18ucs056@lnmiit.ac.in>
2021-06-06 19:10:27 +0530

    A

--- StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/mainActivities/PrivacyPolicy.java
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/mainActivities/PrivacyPolicy.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -22,8 +24,10 @@ public class PrivacyPolicy extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 001;
    private int c1=0,c2=0,c3=0,c4=0;
-    private LinearLayout rules,newly;
+    private LinearLayout rules,newly,privacy,terms,refund;
     private LinearLayout rulest;
+    private ImageView im11,im22,im33,im44;
+    private WebView privacypolicy,termand,refundpolicy;
     private TextView privacybutton,termsbutton,paymentbutton;
 
     @Override
@@ -33,7 +37,16 @@ public class PrivacyPolicy extends AppCompatActivity {
 
         rules=findViewById(R.id.rule);
         newly=findViewById(R.id.newly);
-
+        im11=findViewById(R.id.im11);
+        im22=findViewById(R.id.im22);
+        im33=findViewById(R.id.im33);
+        im44=findViewById(R.id.im44);
+privacy=findViewById(R.id.privacy);
+terms=findViewById(R.id.terms);
+refund=findViewById(R.id.refund);
+privacypolicy=findViewById(R.id.privacypolicy);
+termand=findViewById(R.id.termsandcondition);
+refundpolicy=findViewById(R.id.refud_policy);
         rulest=findViewById(R.id.ruletext);
 
         rules.setOnClickListener(new View.OnClickListener() {
@@ -44,12 +57,74 @@ public class PrivacyPolicy extends AppCompatActivity {
                 {
                     rulest.setVisibility(View.VISIBLE);
                     newly.setVisibility(View.GONE);
+                    im11.setImageResource(R.drawable.ic_baseline_arrow_drop_up_24);
 
                 }
                 else
                 {
                     rulest.setVisibility(View.GONE);
                     newly.setVisibility(View.VISIBLE);
+                    im11.setImageResource(R.drawable.ic_baseline_arrow_drop_down_24);
+                }
+
+            }
+        });
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c3++;
+                if(c3 %2==1)
+                {
+                    privacypolicy.setVisibility(View.VISIBLE);
+                    newly.setVisibility(View.GONE);
+                    im22.setImageResource(R.drawable.ic_baseline_arrow_drop_up_24);
+
+                }
+                else
+                {
+                    privacypolicy.setVisibility(View.GONE);
+                    newly.setVisibility(View.VISIBLE);
+                    im22.setImageResource(R.drawable.ic_baseline_arrow_drop_down_24);
+                }
+
+            }
+        });
+        terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c2++;
+                if(c2 %2==1)
+                {
+                    termand.setVisibility(View.VISIBLE);
+                    newly.setVisibility(View.GONE);
+                    im33.setImageResource(R.drawable.ic_baseline_arrow_drop_up_24);
+
+                }
+                else
+                {
+                    termand.setVisibility(View.GONE);
+                    newly.setVisibility(View.VISIBLE);
+                    im33.setImageResource(R.drawable.ic_baseline_arrow_drop_down_24);
+                }
+
+            }
+        });
+        refund.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                c1++;
+                if(c1 %2==1)
+                {
+                    refundpolicy.setVisibility(View.VISIBLE);
+                    newly.setVisibility(View.GONE);
+                    im44.setImageResource(R.drawable.ic_baseline_arrow_drop_up_24);
+
+                }
+                else
+                {
+                    refundpolicy.setVisibility(View.GONE);
+                    newly.setVisibility(View.VISIBLE);
+                    im44.setImageResource(R.drawable.ic_baseline_arrow_drop_down_24);
                 }
 
             }
