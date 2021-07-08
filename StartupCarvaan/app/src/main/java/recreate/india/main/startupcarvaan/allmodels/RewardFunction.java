0f110a93555d7992cdf973e932201d6d60c2e82a
sidd2203 <siddharthch22@gmail.com>
2021-07-08 22:25:26 +0530

    reward

---
+++ StartupCarvaan/app/src/main/java/recreate/india/main/startupcarvaan/allmodels/RewardFunction.java
@@ -0,0 +1,42 @@
+package recreate.india.main.startupcarvaan.allmodels;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class RewardFunction {
+    
+    FirebaseFirestore ff=FirebaseFirestore.getInstance();
+    Reward reward=new Reward();
+    Level level=new Level();
+    public RewardFunction(){
+    }
+    public void getRewards(){
+        ff.collection("reward").document("level").addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(value!=null){
+                    level=value.toObject(Level.class);
+
+                }
+                else
+                    level=null;
+            }
+        });
+    }
+    public void getLevels(){
+        ff.collection("reward").document("reward").addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                if(value!=null){
+                    reward=value.toObject(Reward.class);
+                }
+                else
+                    reward=null;
+            }
+        });
+    }
+}
